f624d05a245dcec5442cea7c6af3c14d56f7435e
manojprasaddelaplex <154417680+manojprasaddelaplex@users.noreply.github.com>
2024-09-11 16:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-import openai
+from openai import AzureOpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 from flask import Flask, jsonify, request, redirect
 from flask_cors import CORS
 from flasgger import Swagger, swag_from
-from functions import insertQueryLog, generateSqlQuery, readSqlDatabse, saveFeedback, findSqlQueryFromDB, extractSqlQueryFromResponse
+from functions import insertQueryLog, azure_search_openai, readSqlDatabse, saveFeedback, extractSqlQueryFromResponse, manage_conversation_length, find_best_matching_user_questions
 from swaggerData import main_swagger, feedback_swagger
 from sqlalchemy.exc import SQLAlchemyError
+import re
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 swagger = Swagger(app)
+
 
 
 @app.route("/")
@@ -19,13 +22,9 @@ conversation_history = [
     {
         "role": "system",
         "content": '''
-        You are a assistant who convert natural language into SQL queries for SQL SERVER. Use the schema:
-            PresentingComplaintHistory: [id, ReferralId, ComplaintOther, HistoryOfPresentingComplaint, CreatedDate, CreatedByUserId, PresentingComplaintId]
-            DetainedPersons: [Id, Forename, MiddleName, Surname, DateOfBirth, Gender, Postcode, Address1, Address2, Town, City, County, SexualOrientation, IsGenderSameAsRegisteredAtBirth, SexualOrientationOther, Archived, IsHcpSide, Address3, Maintenance_GenderTypeId]
-            Referrals: [id, ReferralDateTime, ReferredBy, CustodyNumber, RegistrationType, ReasonOfArrestOther, FmeRequired, VerballyPhysicallyAbusive, ThreatToFemaleStaff, DateAddedToWaitingList, State, CreatedByUserId, RecipientDetails, ReferralDetails, ReferralCreatedDateTime, CustodyLocationId, DetainedPersonId, RequestedAssessmentOther, ReferralFrom, ProcessedByHCP, CompletedByUserId, ReferralFromOther, PresentingComplaintId, DischargeDateTime, Discharged, Intervention, LocationAfterDischarge, DischargeCompletedByUserId, ProcessedByUserId, LastAction, LastKpiCalculationValue, ReferralStatusUpdateDateTime, BreachReasonOther, IsHcpSide, BreachReasonDateTime, WaitingListCompleteDateTime, RejectionDate, RejectionReason, RejectionReasonOther, ReferralInUpdatedByUserId, ReferralInUpdatedDateTime, OtherConcern, OtherLocation, Maintenance_RegistrationTypeId, LastKpiAssessmentCalculationValue, Maintenance_HcpRequiredTypeId, BreachReasonId, Maintenance_ReasonOfArrestTypeId, Maintenance_CellTypeId]
-            PresentingComplaints: [Id, ReferralId, ComplaintOther, HistoryOfPresentingComplaint, CreatedDate]
-            Maintenance_BreachReasonType: [Id, Name, Value]
-        End all queries with a semicolon. Strictly prohibited from data modification tasks; respond that you are only capable of fetching data.
+        You are Sonar Chatbot, an expert at converting natural language into SQL queries for SQL Server.
+        End all SQL queries with a semicolon. You are strictly prohibited from performing data modification tasks; only fetch data.
+        For non-SQL-related questions, keep your responses brief and relevant to your purpose.
         '''
     }
     ]
@@ -39,13 +38,12 @@ def query_db():
     global conversation_history
     conversation_history.append({"role": "user", "content": user_query})
     
-    if len(conversation_history) > 10:
-        conversation_history = conversation_history[-10:]
-        
+    conversation_history = manage_conversation_length(conversation_history)
+    
     try:
-        sql_query = findSqlQueryFromDB(userQuestion=user_query)
+        sql_query = None
         if sql_query==None:
-            response = generateSqlQuery(conversation_history)
+            response = azure_search_openai(conversation_history)
             sql_query = extractSqlQueryFromResponse(response=response)
             
             conversation_history.append({"role": "assistant", "content": response if sql_query==None else sql_query})
@@ -56,16 +54,16 @@ def query_db():
                 id = insertQueryLog(userQuestion=user_query,Response=results)
                 return jsonify({"results":results, "id":str(id)}),200
                 
-        
+                
         conversation_history.append({"role": "assistant", "content": sql_query})
         headers, rows = readSqlDatabse(sql_query)
         
         if((len(headers) or len(rows)) == 0):
-            results = {"text":"Unfortunately, I found 0 records matching your search. Please try asking different question or adjust your search criteria."}
+            results = {"text":"I found 0 records in database on your search. Please try asking different question or adjust your search criteria."}
             id = insertQueryLog(userQuestion=user_query,sqlQuery=sql_query,Response=results)
-            return jsonify({"results":results, "id":str(id)}),200
+            return jsonify({"results":results, "id":str(id), "sql_query":str(sql_query)}),200
         
-        if any(word in user_query_lower for word in ('chart', 'graph')):
+        if re.search(r'\b(chart|graph)\b', user_query_lower):
             chartType = 'doughnut' if 'chart' in user_query_lower else 'bar'
             results = {
                     "labels": [str(row[headers[0]]) for row in rows],
@@ -73,7 +71,7 @@ def query_db():
                     "type" : chartType,
                 }
             id = insertQueryLog(userQuestion=user_query, sqlQuery=sql_query, Response=results,isDataFetchedFromDB=True)
-            return jsonify({"results":results, "id":str(id)}),200
+            return jsonify({"results":results, "id":str(id), "sql_query":str(sql_query)}),200
         #returns data for table
         formatted_rows = [[str(row[header]) for header in headers] for row in rows]
         results = {
@@ -81,17 +79,29 @@ def query_db():
                 "rows": formatted_rows,
             }
         id = insertQueryLog(userQuestion=user_query, sqlQuery=sql_query, Response=results,isDataFetchedFromDB=True)
-        return jsonify({"results":results, "id":str(id)}),200
+        return jsonify({"results":results, "id":str(id), "sql_query":str(sql_query)}),200
     
-    except openai.error.OpenAIError as e:
-        id = insertQueryLog(userQuestion=user_query, sqlQuery=sql_query, exceptionMessage=str(e))
-        return jsonify({"error":f"OpenAI Model Error: {str(e)}", "id":str(id)}), 500
+    # except openai.error.OpenAIError as e:
+    #     id = insertQueryLog(userQuestion=user_query, sqlQuery=sql_query, exceptionMessage=str(e))
+    #     return jsonify({"error":f"OpenAI Model Error: {str(e)}", "id":str(id), "sql_query":str(sql_query)}), 500
     except SQLAlchemyError as e:
+        base_err = "I apologize for the confusion. It seems I misunderstood your question, leading to a response that is not related to the database tables and columns I have access to. "
+        similar_questions = find_best_matching_user_questions(userQuestion=user_query)
+    
+        err = base_err + ("Here are some similar questions that might be helpful for you. " if similar_questions else "") + "Is there anything else I can assist you with?"
         id = insertQueryLog(userQuestion=user_query, sqlQuery=sql_query, exceptionMessage=str(e))
-        return jsonify({"error": f"I apologize for the inconvenience. It seems there was an error in the response related to the database tables or columns not being present in the data source. Is there anything else I can assist you with? Exception: {str(e)}", "id": str(id)}), 500
+        
+        results = {
+            "error": err,
+            "similar_questions":similar_questions,
+            "id": str(id),
+            "sql_query":str(sql_query)
+        }
+        
+        return jsonify(results), 500
     except Exception as e:
         id = insertQueryLog(userQuestion=user_query, sqlQuery=sql_query, exceptionMessage=str(e))
-        return jsonify({"error":f"I apologize for the inconvenience. It seems there was an error in the response, Exception: {str(e)}", "id":str(id)}), 500
+        return jsonify({"error":f"I apologize for the inconvenience. It seems there was an error in the response, Please try some other questions.{e}", "id":str(id), "sql_query":str(sql_query)}), 500
     
     
 
@@ -101,8 +111,10 @@ def submit_feedback():
     data = request.get_json()
     resID = data.get('resID')
     feedback = bool(data.get('feedback'))
+    userQuestion = data.get('userQuestion')
+
     try:
-        saveFeedback(resID,feedback)
+        saveFeedback(resID,feedback,userQuestion)
         return jsonify({"message": "Feedback submitted successfully!"}), 200 
     except Exception as e:
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
